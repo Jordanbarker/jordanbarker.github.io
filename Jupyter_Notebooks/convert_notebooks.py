7965e104463933844a75baeb543cbe204ba72068
Jordan Barker <Jordanbarker5@gmail.com>
2024-05-09 17:14:05 -0500

    Moving location and adding new notebook

--- Jupyter_Notebooks/convert_notebooks.py
+++ Jupyter_Notebooks/convert_notebooks.py
@@ -3,14 +3,17 @@ import subprocess
 # List of notebook files
 notebooks = [
     "Accuracy_Metrics.ipynb", 
-    "StatisticalTests.ipynb"
+    "StatisticalTests.ipynb",
+    "TimeSeries.ipynb"
 ]
 # Output directory should be one level up.
 output_dir = "../"
+# output_dir = "/"
 
 # Loop through the list and convert each notebook
 for nb in notebooks:
     command = ["jupyter", "nbconvert", "--to", "html", 
-               f'Jupyter_notebooks/{nb}', "--template", "classic", 
+            #    f'Jupyter_notebooks/{nb}', "--template", "classic",
+               f'{nb}', "--template", "classic",
                "--output-dir", output_dir]
     subprocess.run(command)
